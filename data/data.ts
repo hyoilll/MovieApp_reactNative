d32f081a6cbf5ab7a696b84ccf832ae24d6ed528
李孝一 <hyoil@rikouichinoMacBook-Air.local>
2023-08-15 16:44:17 +0900

    dark mode & light mode

---
+++ data/data.ts
@@ -0,0 +1,2 @@
+export const BLACK_COLOR = "black";
+export const WHITE_COLOR = "white";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from "react-native";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { BLACK_COLOR, WHITE_COLOR } from "../data/data";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const isDarkMode = useColorScheme() === "dark" ? true : false;
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: isDarkMode ? BLACK_COLOR : WHITE_COLOR,
+          },
+          tabBarActiveTintColor: isDarkMode ? WHITE_COLOR : BLACK_COLOR,
+          headerStyle: {
+            backgroundColor: isDarkMode ? BLACK_COLOR : WHITE_COLOR,
+          },
+          headerTintColor: isDarkMode ? WHITE_COLOR : BLACK_COLOR,
+        }}
+      >
         <Tab.Screen name="Movies" component={Movies} />
         <Tab.Screen name="Tv" component={Tv} />
         <Tab.Screen name="Search" component={Search} />
