88c8f8e580d03349b8731224ea6bbaa7b084bf3f
李孝一 <hyoil@rikouichinoMacBook-Air.local>
2023-08-15 18:02:40 +0900

    A simple way to handle darkmode.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -13,6 +13,9 @@ const Tabs = () => {
   const isDarkMode = useColorScheme() === "dark" ? true : false;
 
   return (
+    // import {DarkTheme, DefaultTheme} from "@react-navigation/native";
+    // 上記のthemeで簡単にdark modeを対応できるようになる。
+    // <NavigationContainer theme={DarkTheme}></NacigationContainer>
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
